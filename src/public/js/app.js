00cbbd6c0c7b9957bea41a17a38e637d637cb4aa
jing341 <takio0116@gmail.com>
2021-07-31 12:32:59 +0900

    Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,20 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 function handleOpen() {
   console.log("Connected to Server ✅");
 }
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("open", handleOpen);
@@ -20,8 +26,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
